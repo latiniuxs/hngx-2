d2ebd1835e1e94e131dd5578b4fa6cf9bdf43577
latiniuxs <koredeoladetohun@gmail.com>
2023-09-16 23:44:36 +0100

    updated

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -21,43 +21,42 @@ const MovieCard = ({ movie }) => {
   };
 
   return (
-    <Link to={`/movies/${id}`}>
-    <div  data-testid="movie-card">
-    <div className="bg-white rounded-lg shadow-lg border border-gray-300 hover:shadow-xl hover:border-gray-400 cursor-pointer mb-4 sm:mb-6 md:mb-8 lg:mb-10">
-        <div className="h-96 relative">
-          <img
-            src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
-            alt={title}
-            className="w-full h-full object-cover rounded-t-lg"
-            data-testid="movie-poster"
-          />
-          <div className="absolute top-2 right-2">
+    <div data-testid="movie-card">
+      <Link to={`/movies/${id}`}>
+        <div className="bg-white rounded-lg shadow-lg border border-gray-300 hover:shadow-xl hover:border-gray-400 cursor-pointer mb-4 sm:mb-6 md:mb-8 lg:mb-10">
+          <div className="h-96 relative">
             <img
-              src={favoriteIcon}
-              className={` ${isFavorite ? "bg-red-500 rounded-full" : ""}`}
-              alt={`Favorite Icon for ${title}`}
-              onClick={toggleFavorite}
+              src={`https://image.tmdb.org/t/p/w500/${poster_path}`}
+              alt={title}
+              className="w-full h-full object-cover rounded-t-lg"
+              data-testid="movie-poster"
             />
+            <div className="absolute top-2 right-2">
+              <img
+                src={favoriteIcon}
+                className={` ${isFavorite ? "bg-red-500 rounded-full" : ""}`}
+                alt={`Favorite Icon for ${title}`}
+                onClick={toggleFavorite}
+              />
+            </div>
+          </div>
+          <div className="p-4">
+            <p
+              className="text-gray-600 text-sm truncate"
+              data-testid="movie-release-date"
+            >
+              {utcReleaseYear(release_date)}
+            </p>
+            <h2
+              className="text-lg font-semibold truncate"
+              data-testid="movie-title"
+            >
+              {title}
+            </h2>
           </div>
         </div>
-        <div className="p-4">
-          <p
-            className="text-gray-600 text-sm truncate"
-            data-testid="movie-release-date"
-          >
-            {utcReleaseYear(release_date)}
-          </p>
-          <h2
-            className="text-lg font-semibold truncate"
-            data-testid="movie-title"
-          >
-            {title}
-          </h2>
-        </div>
-      </div>
+      </Link>
     </div>
-      
-    </Link>
   );
 };
 
